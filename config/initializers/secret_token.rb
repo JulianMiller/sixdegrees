690a863814399937e5de7ef96030e7fc1909618a
Julian Miller <me@juliananthonymiller.com>
2012-02-16 18:34:23 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sixdegrees::Application.config.secret_token = '0663a509ebb8c983dbeeb4541d66600328d9db248d2aba679c2843822b40330f5689f5601cabb357f89602b7b940ed247fc1c81ee73a0217e120bc716f3d204e'
